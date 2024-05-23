f5588acb3833874f153ca9c23d98cb54ab0b7f4d
seoyeongchan <115616417+seoyeongchan@users.noreply.github.com>
2024-05-23 17:49:48 +0900

    HelloControllerTest 아직 해결방법을 발견못함..

--- src/test/java/com/jojoldu/com/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/com/springboot/web/HelloControllerTest.java
@@ -1,13 +1,14 @@
 package com.jojoldu.com.springboot.web;
 
+
 import com.jojoldu.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -21,7 +22,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws Exception {
+    public void helloIsReturn() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
